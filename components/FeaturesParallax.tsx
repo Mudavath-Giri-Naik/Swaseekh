9fe5adcfa1560d16eec878dac81c260c547bf044
Giri Naik <mudavathgirinaik444@gmail.com>
2026-06-19 19:37:30 +0530

    feat: make decorative objects.svg sticky in bottom right viewport during parallax scroll

--- components/FeaturesParallax.tsx
+++ components/FeaturesParallax.tsx
@@ -109,10 +109,15 @@ export default function FeaturesParallax() {
     <>
       <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
       <div ref={containerRef} className="relative z-20 pb-32 pt-24 mt-12 w-full max-w-[1440px] mx-auto">
-        <div className="absolute bottom-12 right-4 lg:right-16 z-30 pointer-events-none opacity-90 hidden md:block">
-          <img src="/objects.svg" alt="Decorative objects" className="w-[200px] md:w-[300px] lg:w-[450px] h-auto object-contain" />
+        
+        {/* Sticky decorative image container */}
+        <div className="absolute inset-0 z-30 pointer-events-none hidden md:block">
+          <div className="sticky top-0 h-screen w-full flex items-end justify-end pb-12 pr-4 lg:pr-16">
+            <img src="/objects.svg" alt="Decorative objects" className="w-[200px] md:w-[300px] lg:w-[450px] h-auto object-contain opacity-90" />
+          </div>
         </div>
-        <div className="mx-auto flex flex-col items-center">
+
+        <div className="mx-auto flex flex-col items-center relative z-10">
           {cardItems.map((cardItem, i) => (
             <ParallaxCardItem item={cardItem} key={i} id={i} />
           ))}
