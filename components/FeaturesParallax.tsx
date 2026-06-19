b3ee84e515f52967beb3950c27fb83b01b83a29f
Giri Naik <mudavathgirinaik444@gmail.com>
2026-06-19 19:53:07 +0530

    style: remove bottom padding and aggressively translate SVGs down to touch bottom edge

--- components/FeaturesParallax.tsx
+++ components/FeaturesParallax.tsx
@@ -112,9 +112,9 @@ export default function FeaturesParallax() {
         
         {/* Sticky decorative image container */}
         <div className="absolute inset-0 z-30 pointer-events-none hidden md:block">
-          <div className="sticky top-0 h-screen w-full flex items-end justify-between pb-2 px-0">
-            <img src="/object two.svg" alt="Decorative objects left" className="w-[150px] md:w-[220px] lg:w-[320px] h-auto object-contain opacity-90" />
-            <img src="/objects.svg" alt="Decorative objects right" className="w-[200px] md:w-[300px] lg:w-[450px] h-auto object-contain opacity-90" />
+          <div className="sticky top-0 h-screen w-full flex items-end justify-between px-0 pb-0">
+            <img src="/object two.svg" alt="Decorative objects left" className="w-[150px] md:w-[220px] lg:w-[320px] h-auto object-contain opacity-90 translate-y-4" />
+            <img src="/objects.svg" alt="Decorative objects right" className="w-[200px] md:w-[300px] lg:w-[450px] h-auto object-contain opacity-90 translate-y-4" />
           </div>
         </div>
 
